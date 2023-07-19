4d6b1d8f0aa5e04a030f0a93d2f633d779407040
SakutaTHZ <90145022+Kur0Daisuke@users.noreply.github.com>
2023-07-19 16:02:12 +0630

    client

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import './App.css';
+import WeatherInfo from "./WeatherInfo.js";
+
+
+
+function App() {
+  const [name, setName] = React.useState("");
+  const [data, setData] = React.useState(null);
+  const [staticName, setStatic] = React.useState("");
+  const [isFetchingData, setFechStatus] = React.useState(false);
+
+  const load = async (Toload) => {
+    setFechStatus(true)
+    try {
+      const data = await (await fetch(`/api/${Toload}`)).json()
+      setData(JSON.parse(data.message))
+      setFechStatus(false)
+    } catch (err) {
+      setData("e")
+    }
+    setStatic(Toload)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    load(name)
+  }
+  const buttonClick = (event) => {
+    event.preventDefault();
+    load(staticName)
+  }
+  
+  return (
+    <div className="App">
+      <div className="mainMenu">
+        {/* form */}
+        <form onSubmit={handleSubmit}>
+          <label>Enter location - </label>
+          <input 
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required="required" placeholder='Enter a location'
+            />
+          <input type="submit" />
+          <button onClick={buttonClick}>Refresh</button>
+        </form>
+
+        <div className="Input">
+          <WeatherInfo data={data===null || data===undefined? "e" : data} loading={isFetchingData} name={staticName}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/WeatherInfo.js
@@ -0,0 +1,58 @@
+import "./App.css"
+
+
+function WeatherInfo(params) {
+    const getDayName = (date = new Date(), locale = 'en-US') => {
+        return date.toLocaleDateString(locale, {weekday: 'long'});
+    }
+    console.log(params)
+    return (
+        params.loading ? <>
+            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </>: 
+        params.data === "e" ? <p>Error on finding weaher info of {params.name}</p> :  <>
+        <div className="local">
+            <div className="description">
+                <h1 className="weathername">Weather of {params.name}</h1>
+                <p>Description - {params.data.description}</p>
+            </div>
+            
+            <div className="currentDay">
+                <div className="Row">
+                    <h3>{getDayName(new Date())}</h3>
+                    <p>{params.data.currentConditions.datetime}</p>
+                    <img alt="weather" src={`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/1st%20Set%20-%20Color/${params.data.currentConditions.icon}.png`}/>
+                    
+                    <p>{params.data.currentConditions.conditions}</p>
+                </div>
+                
+                <p>temperature - {params.data.currentConditions.temp}°</p>
+            </div>
+        </div>
+        
+        <div className="nextDays">
+            {
+                params.data.days.map((item, index) => 
+                
+                <>
+                    <div className={`item ${index===0?"first":""}`}>
+                        <h3>{getDayName(new Date(item.datetime))}</h3>
+                        <img alt="weather" src={`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/1st%20Set%20-%20Color/${item.icon}.png`}/>
+                        
+                        {/* <p>{item.conditions}</p> */}
+                        <p>{item.temp}°</p>
+                    </div>
+                </>
+                )
+            }
+            
+        </div>
+        
+        </>
+            
+        
+        
+    )
+}
+
+export default WeatherInfo;
